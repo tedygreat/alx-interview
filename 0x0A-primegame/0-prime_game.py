8b3ed70c842aeefdfad951e05eadb01d99bbcfc7
root <tedygreat@gmail.com>
2023-12-10 01:22:55 -0800

    t

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,52 +1,57 @@
 #!/usr/bin/python3
-""" prime game interview question """
+"""Module defining isWinner function."""
 
 
-def get_first_prime(values):
-    for i in values:
-        if isPrime(i):
-            return [v for v in values if v % i != 0]
-    return False
+def isWinner(x, nums):
+    """Function to get who has won in prime game"""
+    mariaWinsCount = 0
+    benWinsCount = 0
+
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
+
+        if not primesSet:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
 
 
-def isPrime(x):
-    """ check if number is a prime number """
-    if x < 2 or x == 4:
+def is_prime(n):
+    """Returns True if n is prime, else False."""
+    if n < 2:
         return False
-    for i in range(2, x // 2):
-        if x % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
 
-def isWinner(x, nums):
-    """ return name of the player that won the most rounds """
-    if x != len(nums):
-        return None
-    M = {"Turn": True, "Score": 0}
-    B = {"Turn": False, "Score": 0}
-    round = 0
-    while (x):
-        B["Turn"] = False
-        M["Turn"] = True
-        if round >= len(nums):
-            round = 0
-        current = [x for x in range(1, nums[round] + 1)]
-        while(len(current) > 1):
-            if get_first_prime(current):
-                current = get_first_prime(current)
-                if M["Turn"]:
-                    M["Turn"] = False
-                    B["Turn"] = True
-                else:
-                    B["Turn"] = False
-                    M["Turn"] = True
-        if M["Turn"]:
-            B["Score"] += 1
-        else:
-            M["Score"] += 1
-        round += 1
-        x -= 1
-    if B["Score"] < M["Score"]:
-        return 'Maria'
-    return 'Ben'
+def primes_in_range(start, end):
+    """Returns a list of prime numbers between start and end (inclusive)."""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
